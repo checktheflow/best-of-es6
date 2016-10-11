62b78c1cec7c9d8368cf65a372af386fd1dea849
Kim Hogeling <kim.hogeling@s24.com>
2016-10-11 21:38:51 +0200

    [oneliners] new proxy for sorting and table now sortable

--- oneliners/js/app.js
+++ oneliners/js/app.js
@@ -54,61 +54,45 @@ const onelinerIsStrongEnough = (neededStrength = 0) =>
  ***********/
 
 /**
- * Compare puns of two oneliners by alphabet
+ * @param {Number} direction contains 1 or -1 for sorting ascending or descending
  */
-const punSorter = ({ pun: punL = '' }, { pun: punR = ''}) =>
-    punL.toLowerCase() < punR.toLowerCase() ? -1 : 1
+const alphabeticSorter = (strLeft = '', strRight = '', direction = 1) =>
+        (strLeft.toLowerCase() < strRight.toLowerCase() ? -1 : 1) * direction
+
+/**
+ * Compare puns of two oneliners by alphabet
+ * @param {Number} direction contains 1 or -1 for sorting ascending or descending
+ */
+const punSorter = (direction = 1) =>
+    ({ pun: strLeft = '' }, { pun: strRight = ''}) =>
+        alphabeticSorter(strLeft, strRight, direction)
+
+/**
+ * Compare puns of two oneliners by alphabet
+ * @param {Number} direction contains 1 or -1 for sorting ascending or descending
+ */
+const badassSorter = (direction = 1) =>
+    ({ badass: strLeft = '' }, { badass: strRight = ''}) =>
+        alphabeticSorter(strLeft, strRight, direction)
 
 /**
  * Compare strength of two oneliners
+ * @param {Number} direction contains 1 or -1 for sorting ascending or descending
  */
-const strengthSorter = ({ strength: strengthL = 0 }, { strength: strengthR = 0 }) =>
-    strengthL > strengthR ? -1 : 1
-
-
-/*****************
- * Data handlers *
- *****************/
+const strengthSorter = (direction = 1) =>
+    ({ strength: strengthL = 0 }, { strength: strengthR = 0 }) =>
+        (strengthL < strengthR ? -1 : 1) * direction
 
 /**
- * Go through given list of puns,
- * validate them
- * and render them into the 'all-oneliners' node
+ * Decide which sorter should actually be used
  */
-const injectFullListOfOneLiners = (allPuns = []) =>
-    document.getElementById('all-oneliners').innerHTML =
-        allPuns
-        .sort(punSorter)
-        .map(renderPunAsListItem)
-        .join('')
+const sorter = (key = '', direction = 1, caller = 'noname') =>
+    (
+           key === 'pun' && punSorter
+        || key === 'badass' && badassSorter
+        || key === 'strength' && strengthSorter
+    )(direction)
 
-/**
- * Go through given list of puns,
- * validate them,
- * filter them by a given strength,
- * sort them by strength
- * and render them into the 'damn-good-ones' node
- */
-const injectDamnGoodOnesAsCards = (allPuns = []) =>
-    document.getElementById('damn-good-ones').innerHTML =
-        allPuns
-        .filter(onelinerIsStrongEnough(8))
-        .sort(strengthSorter)
-        .map(renderOnelinerAsCard)
-        .join('')
-
-
-const injectTopTenAsTable = (allPuns = []) =>
-    document.getElementById('top-ten').innerHTML =
-        `<tbody>
-        <tr><th>Pun</th><th>Badass</th><th>Strength</th></tr>
-        ${allPuns
-        .filter(onelinerIsValid)
-        .sort(strengthSorter)
-        .splice(0, 10)
-        .map(renderOnelinerAsTableRow)
-        .join('')}
-        </tbody>`
 
 
 /*********
@@ -119,6 +103,12 @@ const injectTopTenAsTable = (allPuns = []) =>
 const oneliners = {
     puns: []
 }
+const tableSort = {
+    currentKey: 'pun',
+    currentDirection: 1,
+    allowedKeys: ['currentKey', 'currentDirection'],
+    allowedTableHeads: ['pun', 'badass', 'strength']
+}
 
 /**
  * pass updates through here, to automatically validate
@@ -127,22 +117,38 @@ const oneliners = {
 const onelinersProxy = new Proxy(oneliners, {
     set(target, name, value) {
         if (name !== 'puns') {
-            throw new Error(`You may only set the puns attribute of the oneliners object 🙈`)
+            throw new Error('🙈 You may only set the puns attribute of the oneliners object')
         }
-
         if (!(value instanceof Array)) {
-            throw new Error('`puns` needs to be an array 🙈')
+            throw new Error('🙈 `puns` needs to be an array')
         }
-
-        // console.log(value)
-
         if (!value.every(onelinerIsValid)) {
-            throw new Error('One of the given one-liners is missing either a `badass`, `strength` or `pun` attribute 🙈')
+            throw new Error('🙈 One of the given one-liners is missing either a `badass`, `strength` or `pun` attribute')
         }
 
-        injectFullListOfOneLiners(value)
-        injectDamnGoodOnesAsCards(value)
-        injectTopTenAsTable(value)
+        target[name] = value
+
+        injectTopTenAsList(Array.from(target.puns))
+        injectDamnGoodOnesAsCards(Array.from(target.puns))
+        injectAllAsTable(Array.from(target.puns), tableSort)
+    }
+})
+
+const tableSortProxy = new Proxy(tableSort, {
+    set(target, name, value) {
+        if (!target.allowedKeys.includes(name)) {
+            throw new Error(`🙈 You may only set the following attributes of the tableSort object: [${tableSort.allowedKeys.join(', ')}] `)
+        }
+        if (name === 'currentKey' && (typeof value !== 'string' || !target.allowedTableHeads.includes(value))) {
+            throw new Error(`🙈 currentKey needs to be a string and one of the following options: [${target.allowedTableHeads.join(', ')}] `)
+        }
+        if (name === 'currentDirection' && ![1, -1].includes(value)) {
+            throw new Error('🙈 currentDirection needs to either 1 or -1 ')
+        }
+
+        target[name] = value
+
+        injectAllAsTable(Array.from(oneliners.puns), target)
     }
 })
 
@@ -151,8 +157,69 @@ const onelinersProxy = new Proxy(oneliners, {
  * Application logic *
  *********************/
 
+/**
+ * Go through given list of puns,
+ * validate them
+ * and render them into the 'all-oneliners' node
+ */
+const injectTopTenAsList = (puns = []) =>
+    document.getElementById('top-ten').innerHTML =
+        puns
+        .sort(sorter('strength', -1, 'list'))
+        .slice(0, 10)
+        .map(renderPunAsListItem)
+        .join('')
+
+/**
+ * Go through given list of puns,
+ * validate them,
+ * filter them by a given strength,
+ * sort them by strength
+ * and render them into the 'damn-good-ones' node
+ */
+const injectDamnGoodOnesAsCards = (puns = []) =>
+    document.getElementById('all-good-ones').innerHTML =
+        puns
+        .filter(onelinerIsStrongEnough(9))
+        .sort(sorter('strength', -1, 'cards'))
+        .map(renderOnelinerAsCard)
+        .join('')
+
+/**
+ * @param {Array} puns A list of pun objects
+ * @param {Object} tableSort The state of how to sort the table
+ */
+const injectAllAsTable = (puns = [], { currentKey:key = 'pun', currentDirection:direction = 1 } = {}) =>
+    document.getElementById('all-oneliners').tBodies[0].innerHTML =
+        `${puns
+        .sort(sorter(key, direction))
+        .map(renderOnelinerAsTableRow)
+        .join('')}`
+
+
+/**
+ * Asynchronous function to fetch oneliners which returns a promise on which will be waited untill it is resolved
+ */
 async function getAndInjectOneliners () {
     onelinersProxy.puns = await fetch('/oneliners/oneliners.json').then(d => d.json())
 }
 
+// Ready, set, go!
 getAndInjectOneliners()
+
+/**
+ * Either change direction if clicked the column header which is already sorted by
+ * or if clicked on a different column header sort by that criteria instead.
+ * @param {MouseEvent} event The mouse event
+ */
+function sortByClickedTableHeader(event) {
+    if (tableSortProxy.currentKey === this.id) {
+        tableSortProxy.currentDirection = tableSortProxy.currentDirection * -1
+    } else {
+        tableSortProxy.currentKey = this.id
+    }
+}
+
+document.getElementById('pun').addEventListener('click', sortByClickedTableHeader)
+document.getElementById('badass').addEventListener('click', sortByClickedTableHeader)
+document.getElementById('strength').addEventListener('click', sortByClickedTableHeader)
